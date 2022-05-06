c8e9f6d15d518bc39fae2b5dc81b9594f64f36d9
Shang Liang <shang@wewearglasses.com>
2022-05-06 12:24:25 +0800

    done

--- README.md
+++ README.md
@@ -1,1 +1,133 @@
-# data-cleaning-course-project
+# Data Cleaning Course Project
+
+
+## Usage
+* Clone the repo
+* Open terminal/power shell and navigate into the cloned folder
+* Run R interactive mode
+* Include the R script with command `source('run_analysis.R')`
+* Call `combine_data()` to  combine the test and train data based on the requirements from steps 1-4
+* Call `summerise_data()` to create the other dataset based on the requirements from step 5
+
+
+## Requirement
+The script uses `dplr` package for `summerise_data` function
+
+
+## Codebook
+Column `subject_id` means which person(test subject) provided the measurement
+Column `activity` means what kind of activity is the test subject is performing when the measurement is taken
+
+The remaining columns are inherited from the original dataset `features_info.txt`. 
+The following descriptions are adapted from the original file.
+
+The features selected for this database come from the accelerometer and gyroscope 3-axial raw signals tAcc-XYZ and tGyro-XYZ. These time domain signals (prefix 't' to denote time) were captured at a constant rate of 50 Hz. Then they were filtered using a median filter and a 3rd order low pass Butterworth filter with a corner frequency of 20 Hz to remove noise. Similarly, the acceleration signal was then separated into body and gravity acceleration signals (tBodyAcc-XYZ and tGravityAcc-XYZ) using another low pass Butterworth filter with a corner frequency of 0.3 Hz. 
+
+Subsequently, the body linear acceleration and angular velocity were derived in time to obtain Jerk signals (tBodyAccJerk-XYZ and tBodyGyroJerk-XYZ). Also the magnitude of these three-dimensional signals were calculated using the Euclidean norm (tBodyAccMag, tGravityAccMag, tBodyAccJerkMag, tBodyGyroMag, tBodyGyroJerkMag). 
+
+Finally a Fast Fourier Transform (FFT) was applied to some of these signals producing fBodyAcc-XYZ, fBodyAccJerk-XYZ, fBodyGyro-XYZ, fBodyAccJerkMag, fBodyGyroMag, fBodyGyroJerkMag. (Note the 'f' to indicate frequency domain signals). 
+
+These signals were used to estimate variables of the feature vector for each pattern:  
+'-XYZ' is used to denote 3-axial signals in the X, Y and Z directions.
+
+tBodyAcc-XYZ
+tGravityAcc-XYZ
+tBodyAccJerk-XYZ
+tBodyGyro-XYZ
+tBodyGyroJerk-XYZ
+tBodyAccMag
+tGravityAccMag
+tBodyAccJerkMag
+tBodyGyroMag
+tBodyGyroJerkMag
+fBodyAcc-XYZ
+fBodyAccJerk-XYZ
+fBodyGyro-XYZ
+fBodyAccMag
+fBodyAccJerkMag
+fBodyGyroMag
+fBodyGyroJerkMag
+
+
+Additional vectors obtained by averaging the signals in a signal window sample. These are used on the angle() variable:
+
+gravityMean
+tBodyAccMean
+tBodyAccJerkMean
+tBodyGyroMean
+tBodyGyroJerkMean
+
+The set of variables that were estimated from these signals are: 
+
+mean(): Mean value
+std(): Standard deviation
+
+## Column names
+* subject_id
+* activity
+* tBodyAcc-mean()-X
+* tBodyAcc-mean()-Y
+* tBodyAcc-mean()-Z
+* tBodyAcc-std()-X
+* tBodyAcc-std()-Y
+* tBodyAcc-std()-Z
+* tGravityAcc-mean()-X
+* tGravityAcc-mean()-Y
+* tGravityAcc-mean()-Z
+* tGravityAcc-std()-X
+* tGravityAcc-std()-Y
+* tGravityAcc-std()-Z
+* tBodyAccJerk-mean()-X
+* tBodyAccJerk-mean()-Y
+* tBodyAccJerk-mean()-Z
+* tBodyAccJerk-std()-X
+* tBodyAccJerk-std()-Y
+* tBodyAccJerk-std()-Z
+* tBodyGyro-mean()-X
+* tBodyGyro-mean()-Y
+* tBodyGyro-mean()-Z
+* tBodyGyro-std()-X
+* tBodyGyro-std()-Y
+* tBodyGyro-std()-Z
+* tBodyGyroJerk-mean()-X
+* tBodyGyroJerk-mean()-Y
+* tBodyGyroJerk-mean()-Z
+* tBodyGyroJerk-std()-X
+* tBodyGyroJerk-std()-Y
+* tBodyGyroJerk-std()-Z
+* tBodyAccMag-mean()
+* tBodyAccMag-std()
+* tGravityAccMag-mean()
+* tGravityAccMag-std()
+* tBodyAccJerkMag-mean()
+* tBodyAccJerkMag-std()
+* tBodyGyroMag-mean()
+* tBodyGyroMag-std()
+* tBodyGyroJerkMag-mean()
+* tBodyGyroJerkMag-std()
+* fBodyAcc-mean()-X
+* fBodyAcc-mean()-Y
+* fBodyAcc-mean()-Z
+* fBodyAcc-std()-X
+* fBodyAcc-std()-Y
+* fBodyAcc-std()-Z
+* fBodyAccJerk-mean()-X
+* fBodyAccJerk-mean()-Y
+* fBodyAccJerk-mean()-Z
+* fBodyAccJerk-std()-X
+* fBodyAccJerk-std()-Y
+* fBodyAccJerk-std()-Z
+* fBodyGyro-mean()-X
+* fBodyGyro-mean()-Y
+* fBodyGyro-mean()-Z
+* fBodyGyro-std()-X
+* fBodyGyro-std()-Y
+* fBodyGyro-std()-Z
+* fBodyAccMag-mean()
+* fBodyAccMag-std()
+* fBodyBodyAccJerkMag-mean()
+* fBodyBodyAccJerkMag-std()
+* fBodyBodyGyroMag-mean()
+* fBodyBodyGyroMag-std()
+* fBodyBodyGyroJerkMag-mean()
+* fBodyBodyGyroJerkMag-std()
--- run_analysis.R
+++ run_analysis.R
@@ -1,56 +1,104 @@
+# Make sure dplyr package is installed
+if(!require('dplyr')) install.packages('dplyr')
+library(dplyr)
+
 
 # The script is broken into multiple functions, step by step
-# so it is easier to test and repeat centain steps
+# so it is easier to test and repeat each step
+# combine_data function fulfills the requirement of step 1-4
+# summerise_data function fulfills the requirement of step 5
 
+data_file <- "data.zip"
 data_folder <- "data"
 
-# some utility functions used by other functions
+# Add a debugging toggle
+# When debugging is True, only read the test data to save time
+is_debugging <- TRUE
+
+# Download and extract the data file
+util_download_data <- function() {
+  # if zip file is not downloaded, download the file
+  if (!file.exists(data_file)) {
+    download.file(
+      "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
+      destfile = data_file
+    )
+  }
+  
+  # if the zip file is not expanded, unzip it
+  if (!file.exists(data_folder)) {
+    unzip(data_file, exdir = ".")
+    file.rename("UCI HAR Dataset", data_folder)
+  }
+}
+
+# Utility function that read the same file from both test and train folder
+# then join them together into one data frame
 util_read_both <- function(filename) {
-    test <- read.table(file.path(data_folder, "test", paste(filename, "test.txt", sep = "_")))
-    # train <- read.table(file.path(data_folder, "train", paste(filename, "train.txt", sep = "_")))
-    # rbind(train, test)
-    test
+  
+  # Generate full path to the -test.txt file
+  filename<-paste(filename, 'test.txt', sep = '_')
+  test_file <- file.path(data_folder,  'test', filename )
+  
+  # Read the test file
+  test_df <- read.table(test_file)
+  
+  # Terminate the script here to skip reading the training file
+  if (is_debugging)
+    return(test_df)
+  
+  # Swap the 'test' path to 'train' path 
+  train_file <- gsub('test', 'train', test_file)
+  # Read the rain file
+  train_df <-read.table(train_file)
+  
+  # Combine the 2 df together 
+  rbind(train_df, test_df)
 }
 
-
-
-
-download_data <- function() {
-    dest_file <- "data.zip"
-
-    # if zip file is not downloaded, download the file
-    if (!file.exists(dest_file)) {
-        download.file(
-            "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", # nolint
-            destfile = dest_file
-        )
-    }
-
-    # if the zip file is not expanded, unzip it
-    if (!file.exists(data_folder)) {
-        unzip(dest_file, exdir = ".")
-        file.rename("UCI HAR Dataset", data_folder)
-    }
-}
-
-
+# Combine test and train measurements
+# Select mean and std features only
+# Add subject_id and activity columns as a complete data frame
 combine_data <- function() {
-    subject <- util_read_both("subject")
-    names(subject) <- c("subject_id")
-    activity <- util_read_both("y")
-    names(activity) <- c("activity")
-    activity_labels<-read.table(file.path(data_folder,'activity_labels.txt'))
-    activity$activity<-factor(activity$activity,levels = activity_labels[1],labels = activity_labels[2])
+  # Download and extract data
+  util_download_data()
+  # Read subject ids for each measurement
+  subject <- util_read_both("subject")
+  # Rename the column as subject_id, a more meaningful name
+  names(subject) <- c("subject_id")
+  
+  # Read activity ids for each measurement
+  activity <- util_read_both("y")
+  # Rename the column as activity, a more meaningful name
+  names(activity) <- c("activity")
+  
+  # Read activity labels
+  activity_labels <-
+    read.table(file.path(data_folder, 'activity_labels.txt'))
+  activity[, 1] <- tolower(activity_labels[activity[, 1], 2])
+  
+  # Read features list
+  features <- read.table(file.path(data_folder, "features.txt"))
+  # Filter features with -mean and -std in their name, but not meanFreq
+  selected_features <-
+    grep("(-mean|-std)(?!Freq)", features[, 2], perl = T)
+  
+  
+  # Read measurement
+  measurement <- util_read_both("X")
+  # Extracted selected features
+  selected_measurement <- measurement[, selected_features]
+  
+  # Give feature columns same names as the original data
+  names(selected_measurement) <- features[selected_features, 2]
+  
+  # Join all data together as 1 dataset
+  cbind(subject, activity, selected_measurement)
+}
 
-
-    # measurement <- util_read_both("X")
-    # Filter features with -mean and -std in their name
-    features <- read.table(file.path(data_folder, "features.txt"))
-    selected_features <- grep("(-mean)|(-std)", features[, 2])
-
-    # selected_measurement <- measurement[, selected_features]
-    # names(selected_measurement) <- features[selected_features, 2]
-
-    combined <- merge(subject, activity)
-    print(head(combined))
+# Summarise the combined data based on step 5 requirement
+summarise_data<-function(){
+  combine_data() %>%
+    group_by(subject_id,activity) %>%
+    summarise_all(mean)
 }
